16a02feda409cb463c61255afe2a2d2bdc5d4dd4
COR Students <corstudents@Matts-MacBook-Pro-108.local>
2019-02-16 00:54:17 -0500

    added models directory. added user schema and users route

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DbConfig
 const db = require("./config/keys").mongoURI;
 
 //Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 app.get("/", (req, res) => res.send("Hello"));

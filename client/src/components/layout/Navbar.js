d58023729af30b58b61dfb447088dc8bc7d391ed
Kamaria <kamariapm@gmail.com>
2019-02-23 16:54:43 -0500

    added client forlder, with new src folders

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  //render is a lifecycle method which will "render" whatever is in the {}
+  //return whatever is in render -- this is for JSX
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <a className="navbar-brand" href="landing.html">
+            realDevsMeet
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="profiles.html">
+                  {" "}
+                  Developers
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="register.html">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;

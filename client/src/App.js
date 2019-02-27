ce3c4fe5080ed720372dfce9ba82192d41d0945e
Kamaria <kamariapm@gmail.com>
2019-02-27 00:07:41 -0500

    added validdation/is-empty, set auth token, completed login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -7,10 +13,17 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
-import { Provider } from "react-redux";
-import store from "./store";
-
 import "./App.css";
+
+//Check for token
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //call set current user aciton and is authenticate
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-//Register User
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+
+// Register User
 //dealing with async data--getting info from backend this is where redux-thunk comes in
 export const registerUser = (userData, history) => dispatch => {
   axios
@@ -13,4 +16,37 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to ls
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };

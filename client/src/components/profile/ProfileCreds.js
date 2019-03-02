a8b518311b07dc822ffd2bd0546e8f18f5f3e3c6
Kamaria <kamariapm@gmail.com>
2019-03-02 17:23:54 -0500

    created route to get profile by handle and profile subcompnents

---
+++ client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ProfileCreds extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TO_DO: Profile Creds</h1>
+      </div>
+    );
+  }
+}
+export default ProfileCreds;

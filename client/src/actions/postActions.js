8eb87491d2e2f5e17bbd9c118d6bf13fa685e051
Kamaria <kamariapm@gmail.com>
2019-03-04 21:37:16 -0500

    created commentform component and made action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -108,6 +108,24 @@ export const removeLike = id => dispatch => {
     );
 };
 
+//Add comment
+export const addComment = (postId, commentData) => dispatch => {
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return {

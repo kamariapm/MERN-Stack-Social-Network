0d139eeef2c1a3c46f58f4522a34d055b79361c2
Kamaria <kamariapm@gmail.com>
2019-03-11 10:30:12 -0400

    changed storejs

--- client/src/store.js
+++ client/src/store.js
@@ -17,34 +17,44 @@
 
 // export default store;
 
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers";
+
+// const initialState = {};
+
+// const middleware = [thunk];
+
+// if (
+//   window.navigator.userAgent.includes("Chrome") &&
+//   !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(
+//     window.navigator.userAgent
+//   )
+// ) {
+//   var store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(
+//       applyMiddleware(...middleware),
+//       window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//         window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+//   );
+// } else {
+//   var store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(applyMiddleware(...middleware))
+//   );
+// }
+// export default store;
+
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import reducers from "./reducers";
 
-const initialState = {};
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const middleware = [thunk];
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 
-if (
-  window.navigator.userAgent.includes("Chrome") &&
-  !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(
-    window.navigator.userAgent
-  )
-) {
-  var store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else {
-  var store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware))
-  );
-}
 export default store;

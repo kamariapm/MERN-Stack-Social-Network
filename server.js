f0c754efdd20c0ce747ae25d7fd3f909fa8e67c0
Kamaria <kamariapm@gmail.com>
2019-03-11 10:17:46 -0400

    changed serverjs

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.use(express.static(__dirname + "/src"));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }

cb9e5cf9b36cb528616d094370ae4ef94597099b
Devanshu Vashishtha <sharmadevanshu452@gmail.com>
2021-04-04 22:32:13 +0530

    Update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true ,useFindAndModify: false, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 

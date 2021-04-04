7c7b94e749953a8ec428826598672b3e6f26a876
Devanshu Vashishtha <sharmadevanshu452@gmail.com>
2021-04-04 23:01:48 +0530

    updated

--- server.js
+++ server.js
@@ -38,10 +38,9 @@ app.use("/api/posts", posts);
 if (process.env.NODE_ENV === "production") {
   //set static folder
   app.use(express.static("client/build"));
-  app.use(express.static(__dirname + "/src"));
 
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
